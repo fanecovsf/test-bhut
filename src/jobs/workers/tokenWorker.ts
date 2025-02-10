2df4bfd9597666b3871c6a9cf223304ef52ff8f8
fanecovsf <gustavo_h07@hotmail.com>
2025-02-10 19:20:30 -0300

    feat: ajustes e final commit

--- src/jobs/workers/tokenWorker.ts
+++ src/jobs/workers/tokenWorker.ts
@@ -4,19 +4,14 @@ import GenerateToken from "../GenerateToken";
 import RefreshToken from "../RefreshToken";
 
 new Worker('token-queue', async (job: Job) => {
-    try {
-        switch (job.name) {
-            case "generateToken":
-                new GenerateToken().handler()
-                break
+    switch (job.name) {
+        case "generateToken":
+            new GenerateToken().handler()
+            break
 
-            case "refreshToken":
-                new RefreshToken().handler()
-        }
-        
-    } catch (error) {
-        console.log('error')
-    }
-    
+        case "refreshToken":
+            new RefreshToken().handler()
+            break
+    } 
 },
     { connection: redisConfig, concurrency: 1 })
--- src/jobs/workers/vehicleWorker.ts
+++ src/jobs/workers/vehicleWorker.ts
@@ -3,16 +3,10 @@ import { Worker, Job } from "bullmq";
 import VehicleLogCreation from "../VehicleLogCreation";
 
 new Worker('vehicle-queue', async (job: Job) => {
-    try {
-        switch (job.name) {
-            case "vehicleLog":
-                new VehicleLogCreation().handler(job.data)
-                break
-        }
-        
-    } catch (error) {
-        console.log('error')
+    switch (job.name) {
+        case "vehicleLog":
+            new VehicleLogCreation().handler(job.data)
+            break
     }
-    
 },
     { connection: redisConfig, concurrency: 1 })
